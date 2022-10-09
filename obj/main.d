ea240e3c4f6e8e4fc8d36e20f15d2f766d0f6c53
Anna Gawrysiak <70595292+AnnaGawrysiak@users.noreply.github.com>
2022-10-09 20:41:31 +0200

    Delete main.d

--- obj/main.d
+++
@@ -1,9 +0,0 @@
-obj/main.o: src/main.cpp src/FancyRectangle.hpp src/Point.hpp \
- src/RegularRectangle.hpp src/Rectangle.hpp src/PrinterLibrary.hpp \
- src/FancyToRegular.hpp
-src/FancyRectangle.hpp:
-src/Point.hpp:
-src/RegularRectangle.hpp:
-src/Rectangle.hpp:
-src/PrinterLibrary.hpp:
-src/FancyToRegular.hpp:
